f4906e53833588996946b79dcc24faf449ec9b04
Saurav Thakur <sauravth2003@gmail.com>
2023-12-05 15:35:36 +0530

    Adds the strings

--- Day_1/P1.py
+++ Day_1/P1.py
@@ -1,4 +1,5 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
-print("Hello World!\nHello World!")
+print("print('what to print')") #Prints the string
+print("Hello World!\nHello World!") #\n for newline character
+print("Hello" + " " + "World!") #Adds the strings
