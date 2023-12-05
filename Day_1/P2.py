031ae938088844f8e040cc80e43d56d6cc41f903
Saurav Thakur <sauravth2003@gmail.com>
2023-12-05 16:03:54 +0530

    Improved

--- Day_1/P2.py
+++ Day_1/P2.py
@@ -1,5 +1,5 @@
 #Debugging the errors
 print ("Day 1 - String Manipulation")
-print ("String Concatenation is done with the " + "+" + "sign.")
+print ('String Concatenation is done with the "+" sign.')
 print ('e.g. print("Hello " + "World!")')
 print ("New lines can be created with a backlash and n.")
