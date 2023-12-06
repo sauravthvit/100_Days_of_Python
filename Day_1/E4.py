df4d7349ff8e2b93e5ca688d55e92d7ddcba8e22
Saurav Thakur <sauravth2003@gmail.com>
2023-12-06 22:46:18 +0530

    Conversion of data type added

---
+++ Day_1/E4.py
@@ -0,0 +1,10 @@
+num_char = len(input("What's your name?\n"))
+new_num_char = str(num_char)
+print("Your name contains " + new_num_char + " characters")
+
+a = float(123)
+print(type(a)) #type tells you about the data type of the element.
+
+print(70 + float("100.5"))
+
+#float and str convert the data type to a string and floating point numbers respectively.
