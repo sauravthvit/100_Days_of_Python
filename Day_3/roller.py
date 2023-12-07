7d55c7ada5dc218b6dd2b02ed8b06cb634e90ce2
Saurav Thakur <sauravth2003@gmail.com>
2023-12-07 09:12:46 +0530

    add day 3

---
+++ Day_3/roller.py
@@ -0,0 +1,11 @@
+print("Welcome to the rollercoaster !")
+height = int(input("What is your height in cm?: "))
+
+#if-else statements
+
+if height >= 120:
+    print("Emjoy the ride!")
+else:
+    print("Sorry, you need to be taller to ride it.")
+    
+# == is used to check equality and = is used to assign value to the variable.
