08c00f773e9ff75491efd5d736cdf206eae314be
Saurav Thakur <sauravth2003@gmail.com>
2023-12-07 09:11:22 +0530

    Branched the days

--- Day_1/P5.py
+++
@@ -1,8 +0,0 @@
-two_digit_number = input("Type a two digit number: ")
-
-#Subscripting
-first_digit_number = int(two_digit_number[0])
-second_digit_number = int(two_digit_number[1])
-
-result = first_digit_number + second_digit_number
-print(result)
--- Day_1/roller.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the rollercoaster !")
-height = int(input("What is your height in cm?: "))
-
-#if-else statements
-
-if height >= 120:
-    print("Emjoy the ride!")
-else:
-    print("Sorry, you need to be taller to ride it.")
