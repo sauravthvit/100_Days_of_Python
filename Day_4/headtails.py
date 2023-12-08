63434745b4ec3c30a51abf3a8ae0d72665d6f1c6
Saurav Thakur <sauravth2003@gmail.com>
2023-12-08 11:34:59 +0530

    added lists

---
+++ Day_4/headtails.py
@@ -0,0 +1,8 @@
+import random
+
+coin_generator = random.randint(0,1)
+
+if coin_generator == 0:
+    print ("Heads")
+else:
+    print ("Tails")
---
+++ Day_4/lists1.py
@@ -0,0 +1,12 @@
+list = ["1", "2", "3", "4", "5", "6"]
+
+print (list[1]) #Prints element at index 1
+
+list[1] = 24
+
+print (list) #Updates element at index 1 to 24
+
+list.extend([1, 2, 3, 4, 5]) #adds elements at the end of the list
+print (list)
+
+print (list[-1]) #prints the last element
