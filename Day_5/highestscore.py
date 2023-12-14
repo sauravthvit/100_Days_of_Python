3c86e98edfde83dc0253d440b589c4a402bbd590
Saurav Thakur <sauravth2003@gmail.com>
2023-12-14 10:45:24 +0530

    Added highest score and range

---
+++ Day_5/highestscore.py
@@ -0,0 +1,12 @@
+student_scores = input("Input a list of students scores \n").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+# for loop for max function
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+    
+print(f'The highest score in the class is: {highest_score}')
---
+++ Day_5/range.py
@@ -0,0 +1,6 @@
+# Finding the sum of all numbers from 1 to 100
+
+total = 0
+for number in range(1, 101):
+    total += number #every number will be added to the total
+print(total)
